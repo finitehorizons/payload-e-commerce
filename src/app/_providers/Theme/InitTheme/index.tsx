98316dca9e48684c90a85c9961ad0dff746e3dfe
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-12-07 17:34:26 -0600

    Update index.tsx
    bugfix: fix nextJS deployment bug

--- src/app/_providers/Theme/InitTheme/index.tsx
+++ src/app/_providers/Theme/InitTheme/index.tsx
@@ -7,7 +7,6 @@ export const InitTheme: React.FC = () => {
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
     <Script
       id="theme-script"
-      strategy="beforeInteractive"
       dangerouslySetInnerHTML={{
         __html: `
   (function () {
